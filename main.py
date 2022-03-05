1686191e87c89e49437086ecc23a7d98b4ada712
Polsky75 <44414381+Polsky75@users.noreply.github.com>
2022-03-05 12:08:49 +0300

    Minor update

--- main.py
+++ main.py
@@ -47,19 +47,23 @@ def render(screen, text, position, font_size):
     x, y = position
     return screen.blit(text_render, (x, y))
 
-def button(screen, position, text, font_size):
+
+def main():
+    def button(screen, position, text, font_size, isel = True):
         font = pygame.font.SysFont("Sans", font_size)
         text_render = font.render(text, 1, (32, 32, 32))
         x, y, w , h = text_render.get_rect()
-        x, y = position
+        if isel:
+        	x, y = position
+        else:
+        	x = infoObject.current_w / 2 - w / 2
+        	y = position[1]
         pygame.draw.line(screen, (150, 150, 150), (x, y), (x + w , y), 5)
         pygame.draw.line(screen, (150, 150, 150), (x, y - 2), (x, y + h), 5)
         pygame.draw.line(screen, (50, 50, 50), (x, y + h), (x + w , y + h), 5)
         pygame.draw.line(screen, (50, 50, 50), (x + w , y+h), [x + w , y], 5)
         pygame.draw.rect(screen, (190, 190, 200), (x, y, w , h))
         return screen.blit(text_render, (x, y))
-
-def main():
     infoObject = pygame.display.Info()
     screen = pygame.display.set_mode((infoObject.current_w, infoObject.current_h)) 
     font = pygame.font.Font(None, 32)
@@ -84,9 +88,9 @@ def main():
                                 done = True
                         img1 = font.render('Расчёт данных', True, (255, 255, 255))
                         txt_surface = font.render(text, True, color)
-                        screen.fill((50, 105, 0))  
+                        screen.fill((243,12,32))
                         screen.blit(img1, (infoObject.current_w - 870, 10))
-                        b = button(screen, (infoObject.current_w - 870, 200), " Start ", 70)
+                        b = button(screen, (infoObject.current_w - 870, 300), " Start ", 70)
                         bexit = button (screen, (infoObject.current_w - 60, 10), " X ", 50)
                         if event.type == pg.MOUSEBUTTONDOWN:
                             if bexit.collidepoint(event.pos):
@@ -217,7 +221,7 @@ def main():
                                     text = ''
                                     px.loop = px.loop + 1
                             else:
-                                print("Error")
+                                print("Eroor")
                         
                         elif event.key == pg.K_BACKSPACE:
                             text = text[:-1]
@@ -231,49 +235,49 @@ def main():
                 pg.draw.rect(screen, color, input_box, 2)
                 if px.loop < 22:
                         img = font.render('Введите значение PX' + str(px.loop), True, (255, 255, 255))
-                        screen.blit(img, (infoObject.current_w - 890, 200))
+                        screen.blit(img, (infoObject.current_w - 890, 100))
                         if px.loop == 1:
-                            render(screen, "Физико-химические и механические свойства изоляции", (100,100), 40)
+                            render(screen, "Физико-химические и механические свойства изоляции", (100,300), 40)
                         elif px.loop == 2:
-                            render(screen, "Электрические параметры изоляции", (100,100), 40)
+                            render(screen, "Электрические параметры изоляции", (100,300), 40)
                         elif px.loop == 3:
-                            render(screen, "Электрические параметры радиоэлектронных и микропроцессорных устройств", (100,100), 40)
+                            render(screen, "Электрические параметры радиоэлектронных и микропроцессорных устройств", (100,300), 40)
                         elif px.loop == 4:
-                            render(screen, "Физико-химические свойства полимерной изоляции оболочки кабелей и проводов", (100,100), 40)
+                            render(screen, "Физико-химические свойства полимерной изоляции оболочки кабелей и проводов", (100,300), 40)
                         elif px.loop == 5:
-                            render(screen, "Электрические параметры кабелей и проводов", (100,100), 40)
+                            render(screen, "Электрические параметры кабелей и проводов", (100,300), 40)
                         elif px.loop == 6:
-                            render(screen, "Состояние обмоток электрических машин и трансформаторов", (100,100), 40)
+                            render(screen, "Состояние обмоток электрических машин и трансформаторов", (100,300), 40)
                         elif px.loop == 7:
-                            render(screen, "Показатели качества трансформаторного масла", (100,100), 40)
+                            render(screen, "Показатели качества трансформаторного масла", (100,300), 40)
                         elif px.loop == 8:
-                            render(screen, "Толщина стенки бака маслянного трансформатора", (100,100), 40)
+                            render(screen, "Толщина стенки бака маслянного трансформатора", (100,300), 40)
                         elif px.loop == 9:
-                            render(screen, "Количество циклов включения-выключения аппарата вторичных цепей", (100,100), 40)
+                            render(screen, "Количество циклов включения-выключения аппарата вторичных цепей", (100,300), 40)
                         elif px.loop == 10:
-                            render(screen, "Сопротивление контактных соединений аппаратуры управления", (100,100), 40)
+                            render(screen, "Сопротивление контактных соединений аппаратуры управления", (100,300), 40)
                         elif px.loop == 11:
-                            render(screen, "Рабочие характеристики и долговечность стационарных свиницово-кислотных аккумуляторных батарей", (100,100), 40)
+                            render(screen, "Рабочие характеристики и долговечность стационарных свиницово-кислотных аккумуляторных батарей", (100,300), 40)
                         elif px.loop == 12:
-                            render(screen, "Состав электролита аккумуляторных батарей", (100,100), 40)
+                            render(screen, "Состав электролита аккумуляторных батарей", (100,300), 40)
                         elif px.loop == 13:
-                            render(screen, "Воздушные зазоры между статером и ротером электрических машин", (100,100), 40)
+                            render(screen, "Воздушные зазоры между статером и ротером электрических машин", (100,300), 40)
                         elif px.loop == 14:
-                            render(screen, "Зазоры и величина вибрации в подшипниках скольжения электродвигателя", (100,100), 40)
+                            render(screen, "Зазоры и величина вибрации в подшипниках скольжения электродвигателя", (100,300), 40)
                         elif px.loop == 15:
-                            render(screen, "Прочность и вибростойкость оборудования", (100,100), 40)
+                            render(screen, "Прочность и вибростойкость оборудования", (100,300), 40)
                         elif px.loop == 16:
-                            render(screen, "Время и разновременность замыкания и размыкания главных контактов выключателей", (100,100), 40)
+                            render(screen, "Время и разновременность замыкания и размыкания главных контактов выключателей", (100,300), 40)
                         elif px.loop == 17:
-                            render(screen, "Сопротивление постоянному току трубчатых электронагревателей", (100,100), 40)
+                            render(screen, "Сопротивление постоянному току трубчатых электронагревателей", (100,300), 40)
                         elif px.loop == 18:
-                            render(screen, "Срок службы", (100,100), 40)
+                            render(screen, "Срок службы", (100,300), 40)
                         elif px.loop == 19:
-                            render(screen, "Наработка", (100,100), 40)
+                            render(screen, "Наработка", (100,300), 40)
                         elif px.loop == 20:
-                            render(screen, "Характеристики чехла привода СУЗ - ШЭМ", (100,100), 40)
+                            render(screen, "Характеристики чехла привода СУЗ - ШЭМ", (100,300), 40)
                         elif px.loop == 21:
-                            render(screen, "Электрические характеристики датчиков положения привода СУЗ - ШЭМ", (100,100), 40)
+                            render(screen, "Электрические характеристики датчиков положения привода СУЗ - ШЭМ", (100,300), 40)
                         else:
                             print("Error")
                 elif px.loop == 22:
@@ -293,17 +297,17 @@ def main():
                 for event in pygame.event.get():
                         if event.type == pygame.QUIT:
                                 done = True
-                        screen.fill((50, 105, 0))
-                        b1 = button(screen, (infoObject.current_w - 870, 100), "Вариант А", 50)
-                        b2 = button(screen, (infoObject.current_w - 870, 200), "Вариант Б", 50)
-                        b3 = button(screen, (infoObject.current_w - 870, 300), "Вариант В", 50)
+                        screen.fill((243,12,32))
+                        b1 = button(screen, (infoObject.current_w - 870, 100), "Вариант А", 50, False)
+                        b2 = button(screen, (infoObject.current_w - 870, 200), "Вариант Б", 50, False)
+                        b3 = button(screen, (infoObject.current_w - 870, 300), "Вариант В", 50, False)
                         if event.type == pygame.MOUSEBUTTONDOWN:
                                 if b1.collidepoint(event.pos):
-                                                VariantA()
+                                                px.step = 3
                                 elif b2.collidepoint(event.pos):
-                                                VariantB()
+                                                px.step = 4
                                 elif b3.collidepoint(event.pos):
-                                                VariantC()
+                                                px.step = 5
                                 else:
                                         print("Error")
                         bexit = button (screen, (infoObject.current_w - 60, 10), " X ", 50)
@@ -314,7 +318,26 @@ def main():
         
         
         elif px.step == 4:
-                done = True
+            screen.fill((243,12,32))
+            img = font.render('ABOBA', True, (255, 255, 255))
+            bexit = button (screen, (infoObject.current_w - 60, 10), " X ", 50)
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    done = True
+                if event.type == pg.MOUSEBUTTONDOWN:
+                    if bexit.collidepoint(event.pos):
+                        done = True
+
+        elif px.step == 5:
+            screen.fill((123,64,1))
+            img = font.render('ABOBA', True, (255, 255, 255))
+            bexit = button (screen, (infoObject.current_w - 60, 10), " X ", 50)
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    done = True
+                if event.type == pg.MOUSEBUTTONDOWN:
+                    if bexit.collidepoint(event.pos):
+                        done = True
 
 
 
@@ -329,3 +352,4 @@ if __name__ == '__main__':
     pygame.init()
     main()
     pygame.quit()
+subprocess.call(str(path.join(path.dirname(__file__), filename)))
